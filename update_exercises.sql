d63968ff0fa82dd77ab2f387f083fd368147b5fc
Zach Gulde <zachgulde@gmail.com>
2018-05-23 09:49:53 -0500

    Rewrite SELECTs as UPDATEs

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,19 @@
 USE codeup_test_db;
 
-SELECT * FROM albums\G
+SELECT 'Updating sales...' as '--->>> INFO <<<---';
+UPDATE albums
+SET sales = sales * 10;
+SELECT sales FROM albums;
 
-SELECT * FROM albums WHERE release_date < 1980\G
+SELECT 'Updating release_dates...' as '--->>> INFO <<<---';
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+SELECT release_date FROM albums WHERE release_date < 1980;
 
-SELECT * FROM albums WHERE artist = 'Michael Jackson'\G
+SELECT 'Renaming {Michael,Peter} Jackson' as '--->>> INFO <<<---';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums WHERE artist = 'Peter Jackson';
