e133a28100160d4b8a2755d59d306bdd840b723f
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-05-24 14:07:43 -0500

    Add solution for aliases exercise

--- group_by.sql
+++ group_by.sql
@@ -1,18 +1,42 @@
-SELECT DISTINCT first_name
+
+USE employees;
+
+# use DISTINCT to find the unique titles in the titles table. Your results should look like:
+
+SELECT DISTINCT title
+FROM titles;
+
+# query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY
+SELECT last_name
 FROM employees
-LIMIT 100;
+WHERE last_name like 'E%E'
+GROUP BY last_name;
 
-SELECT first_name
+# find unique combinations of first and last name where the last name starts and ends with 'E'
+SELECT CONCAT(first_name, " ", last_name) AS full_name
 FROM employees
-GROUP BY first_name;
+WHERE last_name like 'E%E'
+GROUP BY full_name
 
-SELECT
-  first_name, last_name, count(*) AS repeats
+SELECT last_name
 FROM employees
--- WHERE first_name LIKE 'e%e'
-  GROUP BY first_name, last_name
-ORDER BY repeats DESC;
+WHERE last_name like '%q%'
+AND last_name not like '%qu%'
+GROUP BY last_name;
 
-DESCRIBE salaries;
+# Add a COUNT() to your results, use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT last_name, count(last_name) as "number of people with this last name"
+FROM employees
+WHERE last_name like '%q%'
+AND last_name not like '%qu%'
+GROUP BY last_name;
 
-select * from employees where first_name = 'Ebbe' and last_name = 'Blokdijk';
+# Update your query for 'Irena', 'Vidya', or 'Maya'.
+# Use count(*) and GROUP BY to find the number of employees
+# for each gender with those names.
+SELECT count(*) "Number", gender
+FROM employees
+WHERE first_name IN ('Vidya', 'Irena', 'Maya')
+GROUP BY gender;
+
+
