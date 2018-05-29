477a5fad2a2571bd8c4b5037afcd1c5d83085fd0
Zach Gulde <zachgulde@gmail.com>
2018-05-29 12:14:10 -0500

    Clean up code examples

--- subqueries.sql
+++ subqueries.sql
@@ -5,45 +5,16 @@ FROM dept_manager dm
 JOIN employees e ON dm.emp_no = e.emp_no
 LIMIT 10;
 
--- SELECT first_name, last_name, birth_date
--- FROM employees
--- WHERE emp_no IN (
---     SELECT emp_no
---     FROM dept_manager
--- )
--- LIMIT 10;
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+)
+LIMIT 10;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- When a query has a join in it, but nothing from the joined table is being
+-- selected; rewrite it with a subquery!
 
 -- Codeup records example -- we have separate tables for students and cohorts
 -- Find the names, github usernames, and email addresses for a given cohort
@@ -53,40 +24,11 @@ FROM students s
 JOIN cohorts c ON s.cohort_id = c.id
 WHERE c.name = 'Ulysses';
 
-
--- SELECT name, email, github_username
--- FROM students
--- WHERE cohort_id = (
---     SELECT id FROM cohorts WHERE name = 'Ulysses'
--- );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT name, email, github_username
+FROM students
+WHERE cohort_id = (
+    SELECT id FROM cohorts WHERE name = 'Ulysses'
+);
 
 -- Who manages the customer service department?
 
@@ -98,39 +40,14 @@ JOIN departments d on dm.dept_no = d.dept_no
 WHERE d.dept_name = 'Customer Service'
     AND dm.to_date > now();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > curdate()
+    AND dept_no = (
+        SELECT dept_no FROM departments
+        WHERE dept_name = 'Customer Service'
+    )
+);
