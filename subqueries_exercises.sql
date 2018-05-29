595ee4538e4ce774dc2166a17a2717ad6023793c
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-05-29 16:01:50 -0500

    add subquery exercise solutions and join bonus problem statements.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,20 +1,30 @@
 USE employees;
 
+# General shape of a subquery
+# SELECT punchLineColumn
+# FROM tableA
+# WHERE specificColumnA IN (
+#    SELECT specificColumnA
+#    FROM tableB
+#    WHERE specificColumnB IN (
+#        SELECT specificColumnB
+#        FROM tableC
+#    )
+# )
+
+
 # Find all the employees with the same hire date as employee 101010 using a sub-query.
 # 69 rows
-
-SELECT CONCAT(first_name, ' ', last_name) as "Enmployees with the same hire date as employee number 101010"
+SELECT first_name, last_name
 FROM employees
 WHERE hire_date IN (
-  SELECT hire_date
-  FROM employees
-  WHERE emp_no = 101010
+   SELECT hire_date
+   FROM employees
+   WHERE emp_no = 101010
 );
-
 
 # Find all the titles held by all employees with the first name Aamod.
 # 314 total titles, 6 unique titles
-
 SELECT title
 FROM titles
 WHERE emp_no IN (
@@ -26,12 +36,12 @@ WHERE emp_no IN (
 # Find all the current department managers that are female.
 SELECT first_name, last_name
 FROM employees
-WHERE gender = 'F'
-AND emp_no IN (
+WHERE emp_no IN (
   SELECT emp_no
   FROM dept_manager
   WHERE to_date > NOW()
-);
+)
+AND gender = 'F';
 
 # Find all the department names that currently have female managers.
 SELECT dept_name
@@ -48,13 +58,14 @@ WHERE dept_no IN (
 );
 
 # Find the first and last name of the employee with the highest salary.
+
 SELECT first_name, last_name
 FROM employees
 WHERE emp_no IN (
   SELECT emp_no
   FROM salaries
-  WHERE salary = (
+  WHERE salary IN (
     SELECT MAX(salary)
     FROM salaries
   )
-);
+)
