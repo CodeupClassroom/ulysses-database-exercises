10f8f3b35a1821249847b6dfbd3c2c0fae854eb5
Zach Gulde <zachgulde@gmail.com>
2018-05-29 10:56:01 -0500

    Add more subqueries examples

--- subqueries.sql
+++ subqueries.sql
@@ -1,16 +1,136 @@
--- Codeup records example
-SELECT name, github_username
-FROM users
-WHERE cohort_id = (
-    SELECT id FROM cohorts WHERE name = 'Ulysses'
-);
-
 -- find all the managerial employees without a JOIN
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE emp_no IN (
-    SELECT emp_no
-    FROM dept_manager
-)
+
+SELECT e.first_name, e.last_name, e.birth_date
+FROM dept_manager dm
+JOIN employees e ON dm.emp_no = e.emp_no
 LIMIT 10;
+
+-- SELECT first_name, last_name, birth_date
+-- FROM employees
+-- WHERE emp_no IN (
+--     SELECT emp_no
+--     FROM dept_manager
+-- )
+-- LIMIT 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Codeup records example -- we have separate tables for students and cohorts
+-- Find the names, github usernames, and email addresses for a given cohort
+
+SELECT s.name, s.email, s.github_username
+FROM students s
+JOIN cohorts c ON s.cohort_id = c.id
+WHERE c.name = 'Ulysses';
+
+
+-- SELECT name, email, github_username
+-- FROM students
+-- WHERE cohort_id = (
+--     SELECT id FROM cohorts WHERE name = 'Ulysses'
+-- );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Who manages the customer service department?
+
+SELECT
+    CONCAT(e.last_name, ', ', e.first_name) as name
+FROM employees e
+JOIN dept_manager dm on dm.emp_no = e.emp_no
+JOIN departments d on dm.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service'
+    AND dm.to_date > now();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
